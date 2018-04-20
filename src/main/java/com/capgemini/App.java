2299488b2324863042c772202429777dd03b8bca
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-04-20 12:37:07 +0200

    Generating district and career players

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -10,13 +10,13 @@ public class App {
     {
         System.out.println("Hello World!");
         BattleItem banana = new BattleItem("Banana", 10,5);
-        Contestant firstPerson = new District(10,10,Gender.MALE,10,4,banana);
-        Contestant secondPerson = new District(11,15, Gender.FEMALE,5,10,banana);
-        Contestant[] listOfThem = {firstPerson, secondPerson};
-        ArenaController listOfContestants = new ArenaController("District",listOfThem);
-        listOfContestants.generateDistrictPlayers();
-        System.out.println("End point");
-        
+
+        ArenaController listOfDistrictContestants = new ArenaController("District");
+
+        listOfDistrictContestants.generatePlayers();
+        System.out.println("Test");
+
+
     }
 
 
--- src/main/java/com/capgemini/ArenaController.java
+++ src/main/java/com/capgemini/ArenaController.java
@@ -4,26 +4,28 @@ import java.util.Random;
 
 public class ArenaController {
     private String contestantType;
-    Contestant[] listOfContestants;
 
-    public ArenaController(String contestantType, Contestant[] listOfContestants) {
+    public ArenaController(String contestantType) {
         this.contestantType = contestantType;
-        this.listOfContestants = listOfContestants;
     }
 
     Contestant[] districtPlayersMales = new District[9];
     Contestant[] districtPlayersFemales = new District[9];
-    Contestant[] careerPlayers = new Career[3];
+    Contestant[] careerPlayersMales = new Career[3];
+    Contestant[] careerPlayersFemales = new Career[3];
 
 
     Random random = new Random();
 
-    public void generateDistrictPlayers() {
+    public void generatePlayers() {
         BattleItem banana = new BattleItem("Banana", 10, 5);
 
 
         int arrayLength = districtPlayersMales.length;
         int arrayLength2 = districtPlayersFemales.length;
+        int arrayLength3 = careerPlayersMales.length;
+        int arrayLength4 = careerPlayersFemales.length;
+
 
         for (int i = 0; i < arrayLength; i++) {
             int counter = random.nextInt(100) + 1;
@@ -32,6 +34,7 @@ public class ArenaController {
             int defenseRandom = random.nextInt(100) + 1;
             this.districtPlayersMales[i] = new District(counter, healthRandom, Gender.MALE, attackRandom, defenseRandom, banana);
         }
+
         for (int i = 0; i < arrayLength2; i++) {
             int counter = random.nextInt(100) + 1;
             int healthRandom = random.nextInt(100) + 1;
@@ -39,7 +42,23 @@ public class ArenaController {
             int defenseRandom = random.nextInt(100) + 1;
             this.districtPlayersFemales[i] = new District(counter, healthRandom, Gender.FEMALE, attackRandom, defenseRandom, banana);
         }
+
+        for (int i = 0; i < arrayLength3; i++) {
+            int counter = random.nextInt(100) + 1;
+            int healthRandom = random.nextInt(100) + 1;
+            int attackRandom = random.nextInt(100) + 1;
+            int defenseRandom = random.nextInt(100) + 1;
+            this.careerPlayersMales[i] = new Career(counter, healthRandom, Gender.MALE, attackRandom, defenseRandom, banana);
+        }
+        for (int i = 0; i < arrayLength4; i++) {
+            int counter = random.nextInt(100) + 1;
+            int healthRandom = random.nextInt(100) + 1;
+            int attackRandom = random.nextInt(100) + 1;
+            int defenseRandom = random.nextInt(100) + 1;
+            this.careerPlayersFemales[i] = new Career(counter, healthRandom, Gender.FEMALE, attackRandom, defenseRandom, banana);
+        }
     }
+
     public String getContestantType() {
         return contestantType;
     }
