dda44e4553962d7179896ffe99eb89520be8c31f
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-04-20 14:28:05 +0200

    Made small changes in App class

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -10,21 +10,19 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
         //testing hello world
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
 
         //creating new ArenaController object
-//        ArenaController listOfDistrictContestants = new ArenaController("District");
-//        listOfDistrictContestants.generatePlayers();
-//
-
         ArenaController joinEverything = new ArenaController("Joining");
+
+        //generating players
         joinEverything.generatePlayers();
+
+        //joining all players
         joinEverything.joinAll(joinEverything.districtPlayersMales, joinEverything.careerPlayersFemales, joinEverything.districtPlayersFemales, joinEverything.careerPlayersMales);
 
+        //requesting all alive players
         joinEverything.requestAlivePlayers();
-
-        //testing just ending
-        System.out.println("Test");
 
         //I know it is stupid, but did not have time to implement it in a Game Controller. Just showing how could two players fight. Did not have time for "for" loop as well :(
         joinEverything.allPlayers[0].getHealth();
